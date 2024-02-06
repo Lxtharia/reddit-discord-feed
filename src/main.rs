1e8123871a52919e828187eb8f9d968d3af9b4ff
Lxtharia <87075045+Lxtharia@users.noreply.github.com>
2024-02-06 19:37:54 +0100

    Add async, send first webhook json object

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-use reqwest
+use reqwest;
+use serde_json::json;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Create a http client
-    let client = reqwest::Client.new()
+    let client = reqwest::Client::new();
 
     // process a feed, once
-    process_feed(client, "", "");
+    let _ = process_feed(client, "https://www.reddit.com/r/schkreckl.rss", "https://discord.com/api/webhooks/894348357592559618/bCpUzEfUcZjcx2Gw4T28SQccWCpwrQzn7ssj8_rYJ-H278jZwfXDpBTubexkSMdSdxTe").await;
 
 }
 
 
-fn process_feed(client: reqwest::Client, reddit_url: str, webhook_url: str) {
-    
+async fn process_feed(client: reqwest::Client, reddit_url: &str, webhook_url: &str) -> Result<(),reqwest::Error> {
+    let body = client.get(reddit_url).send()
+        .await?
+        .text()
+        .await?;
 
+    // parsing
 
+    // Creating a json body to send to discord
+    let data = json!({
+        "content" : "Hallo!"
+    });
+
+    let res = client.post(webhook_url)
+        .json(&data)
+        .send()
+        .await?;
+
+    Ok(())
 }
